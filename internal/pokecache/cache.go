fc69b4d6acde52dc349e204312a2ef4babb610e5
dapoulsen <dapoulsen14@gmail.com>
2026-08-22 13:03:07 +0200

    added cache functionality

--- internal/pokecache/cache.go
+++ internal/pokecache/cache.go
@@ -6,8 +6,9 @@ import (
 )
 
 type Cache struct {
-	entries map[string]cacheEntry
-	mu      sync.RWMutex
+	entries  map[string]cacheEntry
+	mu       sync.RWMutex
+	interval time.Duration
 }
 
 type cacheEntry struct {
@@ -17,22 +18,48 @@ type cacheEntry struct {
 
 func NewCache(interval time.Duration) *Cache {
 	c := &Cache{
-		entries: make(map[string]cacheEntry),
+		entries:  make(map[string]cacheEntry),
+		interval: interval,
 	}
 
+	c.reapLoop()
+
+	return c
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	ce := cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+	c.entries[key] = ce
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	ce, ok := c.entries[key]
+	if !ok {
+		return make([]byte, 0), false
+	}
+	return ce.val, true
+}
+
+func (c *Cache) reapLoop() {
 	go func() {
-		ticker := time.NewTicker(interval)
+		ticker := time.NewTicker(c.interval)
 		defer ticker.Stop()
 
 		for range ticker.C {
 			c.mu.Lock()
 			for key, entry := range c.entries {
-				if time.Since(entry.createdAt) > interval {
+				if time.Since(entry.createdAt) > c.interval {
 					delete(c.entries, key)
 				}
 			}
 			c.mu.Unlock()
 		}
 	}()
-	return c
 }
